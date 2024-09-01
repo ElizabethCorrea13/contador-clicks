47b80a29126cb3748859e8e9bba132b784b592f3
ElizabethCorrea13 <155923327+ElizabethCorrea13@users.noreply.github.com>
2024-08-31 21:07:28 -0300

    agregar componentes botones

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import Boton from './componentes/Boton';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
+
+  const manejarClic = () => {
+    console.log('clic')
+  }
+
+  const ReiniciarContador = () => {
+    
+  }
+
   return (
     <div className="App">
       <div className='freecodecamp-logo-contenedor'>
@@ -9,6 +19,16 @@ function App() {
           className='freecodecamp-logo'
           src={freeCodeCampLogo}
           alt='Logo de FreeCodeCamp'/>         
+      </div>
+      <div className='contenedor-principal'>
+        <Boton
+        texto='Clic'
+        esBotonDeClic={true}
+        manejarClic={manejarClic}/>
+        <Boton
+        texto='Reiniciar'
+        esBotonDeClic={false}
+        manejarClic={ReiniciarContador}/>
       </div>
     </div>
   );
