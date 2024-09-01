1eff43a02cfcdad3e7ffd181156e8f5193b59f74
ElizabethCorrea13 <155923327+ElizabethCorrea13@users.noreply.github.com>
2024-08-31 23:23:26 -0300

    funcionalidad contador lista

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import { useState } from 'react';
 
 function App() {
 
+  const [numClics, setNumClics] = useState(0);
+
   const manejarClic = () => {
-    console.log('clic')
-  }
+    setNumClics(numClics + 1);
+  };
 
   const ReiniciarContador = () => {
-    
-  }
+    setNumClics(0);
+  };
 
   return (
     <div className="App">
@@ -23,7 +26,7 @@ function App() {
       </div>
       <div className='contenedor-principal'>
         <Contador
-         
+         numClics={numClics}
         />
         <Boton
         texto='Clic'
